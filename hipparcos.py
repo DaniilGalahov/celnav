826834a547c65c1f8cdf3a06e7632facf88578aa
Daniil Galahov <daniil.galahov@yandex.ru>
2023-10-28 19:54:02 +0300

    tested local almanac source, compared with NA and astroPy
    Signed-off-by: Daniil Galahov <daniil.galahov@yandex.ru>

---
+++ hipparcos.py
@@ -0,0 +1,27 @@
+import requests
+import os
+
+databaseSearchUri="https://hipparcos-tools.cosmos.esa.int/cgi-bin/HIPcatalogueSearch.pl?hipId="
+
+def LoadFor(hpId):
+    with requests.Session() as session:
+        response=session.get(databaseSearchUri + str(hpId)) #MUST be GET request as with Celestrack.
+        if response.status_code != 200:
+            print(response)
+            exit()
+        data=response.text
+        lines=data.split("\n")
+        alpha=delta=mu_alpha=mu_delta=0
+        for line in lines:
+            if line.startswith("H8  :"):
+                alpha=float(line[6:27].strip())
+            if line.startswith("H9  :"):
+                delta=float(line[6:27].strip())
+            if line.startswith("H11 :"):
+                mu_alpha=float(line[6:27].strip())
+            if line.startswith("H12 :"):
+                mu_delta=float(line[6:27].strip())
+        return alpha,delta,mu_alpha,mu_delta
+
+#print(LoadFor(91262))
+    
