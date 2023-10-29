751a09f5f4a2fc7e7fd80051f42d249b648e84e9
Daniil Galahov <daniil.galahov@yandex.ru>
2023-10-30 00:06:22 +0300

    Created and tested ephemerides generator; worked with precision
    Signed-off-by: Daniil Galahov <daniil.galahov@yandex.ru>

--- Tests/precision.py
+++ Tests/precision.py
@@ -23,16 +23,18 @@ print("Vallado GHAAries: ",thetaGMST," Deviation from NA: ",GHAAriesNA-thetaGMST
 
 print("==========")
 
-#J1991.25 is April 2.5625, 1991 TT OR JD 2448349.0625
-#JY 365.25 JD (of 86400 sec)
-
 #h,m,s=TimeToHMS(0.5625*86400)
 #print(h,m,s)
 #J1991_25=JulianDate(1991,4,2,h,m,s)
 #print(J1991_25)
 
+#J1991.25 is April 2.5625, 1991 TT or JD 2448349.0625
+
+#JY is 365.25 JD (of 86400 sec) exact
+#Sidereal year length is 365.256363004 (according to Wikipedia https://en.wikipedia.org/wiki/Sidereal_year)
+
 def PositionFor(alpha0,delta0,mu_alpha,mu_delta,JD):
-    deltatyears=(JD-2448349.0625)/365.25 #J1991.25 is April 2.5625, 1991 TT or JD 2448349.0625
+    deltatyears=(JD-2448349.0625)/365.25 
     alpha=alpha0+((mu_alpha/3600/1000)*cos(radians(delta0))*deltatyears)
     delta=delta0+((mu_delta/3600/1000)*deltatyears)
     return alpha,delta
@@ -48,13 +50,11 @@ deltaAP=Vega.DecAt("2023-10-27T14:00:00")
 print("AstroPy Vega SHA:", shaAP,"Deviation from NA:",shaNA-shaAP)
 print("AstroPy Vega Dec:", deltaAP,"Deviation from NA:",deltaNA-deltaAP)
 
-
 import hipparcos
 import simbad
 
 alpha0,delta0,mu_alpha,mu_delta=hipparcos.LoadDataFor(91262)
 #alpha0,delta0,mu_alpha,mu_delta=simbad.LoadDataFor("Vega")
-#print(alpha0,delta0,mu_alpha,mu_delta)
 alpha,delta=PositionFor(alpha0,delta0,mu_alpha,mu_delta,JulianDate(2023,10,27,14,0,0)+((deltaT/86400.0)/2.0))
 sha=360.0-alpha
 print("Catalog+Vallado Vega SHA:",sha,"Deviation from NA:",shaNA-sha)
@@ -73,7 +73,7 @@ deltaSunAP=Sol.DecAt("2023-10-27T14:00:00")
 print("AstroPy Sun HA:", GHASunAP,"Deviation from NA:",GHASunNA-GHASunAP)
 print("AstroPy Sun Dec:", deltaSunAP,"Deviation from NA:",deltaSunNA-deltaSunAP)
 
-vector_rSun=Sun(JulianDate(2023,10,27,14,0,0)+((deltaT/86400.0)/2.0)) # we may reduce 500 sec (the time of flight of light beam from Sun to Earth) like this: -(500/86400)
+vector_rSun=Sun(JulianDate(2023,10,27,14,0,0)+((deltaT/86400.0)/2.0)) # we should not count speed of light in measurements. With this fix ve achieved almodt perfect coincidence with NA
 vector_vSun=vector([0,0,0])
 #vector_rSunIJK,vector_vSunIJK=IAU2000CIO(vector_rSun,vector_vSun,2023,10,27,HMSToTime(14,0,0),-0.4399619,32,-0.140682,0.333309,GCRF) # we don't need this
 rSunVallado,alphaSunVallado,deltaSunVallado,rdotSunVallado,alphadotSunVallado,deltadotSunVallado=GeocentricRadec(vector_rSun,vector_vSun)
--- ephemeris.py
+++ ephemeris.py
@@ -1,23 +1,36 @@
-import sys
-sys.path.append("..\Astrodynamic")
-
 from numpy import array as vector
-
-from Vallado import JulianDate,Sun,GeocentricRadec
+from Vallado import JulianDate,LSTime,Sun,GeocentricRadec,Moon,PlanetRV
 
 navigationPlanets=["Venus",
                    "Mars",
                    "Jupiter",
                    "Saturn"]
 
-deltaT=32.184+37.0-0.0  #IERS correction for 2023    
+deltaT=32.184+37.0-0.0  #IERS correction for 2022-2023
+
+def CorrectedJulianDate(Y,M,D,h,m,s):
+    return JulianDate(Y,M,D,h,m,s)+((deltaT/86400.0)/2.0) #calculating with IERS correction; this way of calculation produces minimal deviation from NA
 
 def ThetaGMSTAt(Y,M,D,h,m,s):
     thetaLST,thetaGMST=LSTime(JulianDate(Y,M,D,h,m,s),0,0) #calculating without IERS correction because it's already counted in polynominal
     return thetaGMST
 
 def SunAt(Y,M,D,h,m,s):
-    vector_r=Sun(JulianDate(Y,M,D,h,m,s)+((deltaT/86400.0)/2.0)) #calculating with IERS correction; this way of calculation produces minimal deviation from NA
+    vector_r=Sun(CorrectedJulianDate(Y,M,D,h,m,s)) 
+    vector_v=vector([0,0,0])
+    r,alpha,delta,rdot,alphadot,deltadot=GeocentricRadec(vector_r,vector_v)
+    return alpha,delta,r
+
+def MoonAt(Y,M,D,h,m,s):
+    vector_r=Moon(CorrectedJulianDate(Y,M,D,h,m,s))
+    vector_v=vector([0,0,0])
+    r,alpha,delta,rdot,alphadot,deltadot=GeocentricRadec(vector_r,vector_v)
+    return alpha,delta,r
+
+def PlanetAt(planetName,Y,M,D,h,m,s):
+    vector_rPlanetFromSun,vector_vPlanetFromSun=PlanetRV(planetName,CorrectedJulianDate(Y,M,D,h,m,s))
+    vector_rEarthToSun=Sun(CorrectedJulianDate(Y,M,D,h,m,s))
+    vector_r=vector_rPlanetFromSun+vector_rEarthToSun
     vector_v=vector([0,0,0])
     r,alpha,delta,rdot,alphadot,deltadot=GeocentricRadec(vector_r,vector_v)
     return alpha,delta,r
