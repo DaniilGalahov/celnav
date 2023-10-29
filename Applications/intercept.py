629160ea8e4619366b06f2a935382ddb3826b39f
Daniil Galahov <daniil.galahov@yandex.ru>
2023-10-30 00:06:02 +0300

    Moved Applications outside
    Signed-off-by: Daniil Galahov <daniil.galahov@yandex.ru>

--- Applications/intercept.py
+++
@@ -1,7 +0,0 @@
-#simple celestial fix intercept algorithm
-import astrometry as astro
-from trigonometry import cos
-
-observation = astro.Observation("Observation1.cfg")
-print("p, nmi = "+str(round(observation.distance,2)))
-print("Z, deg = "+str(round(observation.Z,2)))
