1dde4ca61c828d09b5a54ebcdeb3af766086f531
Daniil Galahov <daniil.galahov@yandex.ru>
2023-10-30 19:38:32 +0300

    Created celestialobject base class
    Signed-off-by: Daniil Galahov <daniil.galahov@yandex.ru>

---
+++ Tests/test_celestialobject.py
@@ -0,0 +1,24 @@
+import sys
+sys.path.append("..")
+
+from celestialobject import CelestialObject
+import unittest
+
+class test_celestialobject(unittest.TestCase):
+    def test_init(self):
+        Sun=CelestialObject("Sun")
+        self.assertEqual(Sun.name,"Sun")
+        self.assertEqual(Sun.type,"Sun")
+        Moon=CelestialObject("Moon")
+        self.assertEqual(Moon.name,"Moon")
+        self.assertEqual(Moon.type,"Moon")
+        Venus=CelestialObject("Venus")
+        self.assertEqual(Venus.name,"Venus")
+        self.assertEqual(Venus.type,"Planet")
+        Deneb=CelestialObject("Deneb")
+        self.assertEqual(Deneb.name,"Deneb")
+        self.assertEqual(Deneb.type,"Star")
+        
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ celestialobject.py
@@ -0,0 +1,41 @@
+from catalog import navigationStarNames
+
+navigationPlanetNames=["Venus",
+                       "Mars",
+                       "Jupiter",
+                       "Saturn"]
+
+celestialObjectDiameters={"Sun":1392700.0,
+                          "Moon":3474.2,
+                          "Venus":12104.0,
+                          "Mars":6779.0,
+                          "Jupiter":139820.0,
+                          "Saturn":116460.0}
+
+class CelestialObject:
+    def __init__(self, name): #name of celestial object must start from capital letter
+        self.name=name        
+        self.type=None        
+        if self.name=="Sun":
+            self.type="Sun"
+        elif self.name=="Moon":
+            self.type="Moon"
+        elif self.name in navigationPlanetNames:
+            self.type="Planet"
+        elif self.name in navigationStarNames:
+            self.type="Star"
+
+    def GHAAt(self, time):
+        pass
+
+    def DecAt(self, time):
+        pass
+
+    def SHAAt(self, time):
+        pass
+
+    def SDAt(self, time):
+        pass
+
+    def HPAt(self, time):
+        pass
