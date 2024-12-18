d83a698294570773bfe2aa74e05e46e75ee6ca83
Daniil Galahov <daniil.galahov@yandex.ru>
2024-12-18 09:56:44 +0700

    Equation solver refactored, test added
    Signed-off-by: Daniil Galahov <daniil.galahov@yandex.ru>

---
+++ src/equationsolver.py
@@ -0,0 +1,8 @@
+#simple equation solver through Cramer's rule
+def LinEq22(a1,b1,h1,a2,b2,h2):
+    d=(a1*b2)-(a2*b1)
+    dx=(h1*b2)-(h2*b1)
+    dy=(a1*h2)-(a2*h1)
+    x=dx/d
+    y=dy/d
+    return x,y
---
+++ tests/test_equationsolver.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append("..\src")
+
+import equationsolver
+import unittest
+
+class test_equationsolver(unittest.TestCase):
+    def test_LinEq22(self):
+        x,y=equationsolver.LinEq22(1,2,3,2,3,1)
+        self.assertEqual(x,-7)
+        self.assertEqual(y,5)
+
+if __name__ == '__main__':
+    unittest.main()
