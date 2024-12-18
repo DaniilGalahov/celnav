21317a50fbc8c1014ae09b93c13289a6cf202d95
Daniil Galahov <daniil.galahov@yandex.ru>
2024-12-18 11:16:05 +0700

    Minor fixes, added skips for catalog tests
    Signed-off-by: Daniil Galahov <daniil.galahov@yandex.ru>

--- Tests/test_catalog.py
+++ Tests/test_catalog.py
@@ -5,6 +5,7 @@ import catalog
 import unittest
 
 class test_catalog(unittest.TestCase):
+    @unittest.skip("Too long to test it often")
     def test_LoadDataFromSourceFor(self):
         catalog.dataSource=0
         alpha,delta,mu_alpha,mu_delta=catalog.LoadDataFromSourceFor("Vega")
@@ -13,7 +14,7 @@ class test_catalog(unittest.TestCase):
         alpha,delta,mu_alpha,mu_delta=catalog.LoadDataFromSourceFor("Vega")
         self.assertAlmostEqual(alpha,279.23473479,6)
 
-    '''
+    @unittest.skip("Too long to test it often")
     def test_CreateLocalCatalogIn(self):
         catalog.dataSource=0
         catalog.CreateLocalCatalog()
@@ -21,8 +22,8 @@ class test_catalog(unittest.TestCase):
         data=file.read()
         file.close()
         self.assertTrue(data.startswith('{"Acamar": [44.5654818, -40.30473491, -53.53, 25.71],'))
-    '''
 
+    @unittest.skip("Too long to test it often")
     def test_LoadDataFor(self):
         catalog.dataSource=0
         alpha,delta,mu_alpha,mu_delta=catalog.LoadDataFor("Deneb")
--- src/external/math.py
+++ src/external/math.py
@@ -1,5 +1,5 @@
 #math
-from math import fabs, sin, cos, tan, asin, acos, atan, atan2, degrees, radians, isclose
+from math import sqrt, fabs, sin, cos, tan, asin, acos, atan, atan2, degrees, radians, isclose
 
 #numpy
 from numpy import array as vector
