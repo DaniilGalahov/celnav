8c300c15ed5ead03c823c1b61e6ba0f79e8eb6c0
Daniil Galahov <daniil.galahov@yandex.ru>
2024-12-21 23:23:44 +0700

    Astrometry WiP
    Signed-off-by: Daniil Galahov <daniil.galahov@yandex.ru>

--- src/astrometry.py
+++ src/astrometry.py
@@ -1,20 +1,31 @@
-from external.math import sqrt, sin, cos, tan, asin, atan, degrees, radians
+from external.math import sqrt
+import external.math as math
 import almanac
+
 import angle
 
-def CalculateIntercept(phigce,lambdae,Y,M,D,h,m,s,celestialObjectName,deltats,hoh,T,P,limb=-1): #based on method described on http://www.tecepe.com.br/nav/inav_met.htm by Omar Reis && Bowditch
+def CalculateIntercept(phie,lambdae,Y,M,D,h,m,s,celestialObjectName,Hs,aoe,T=10,P=1010.0,limb=-1,IC=0): #based on method described in Bowditch (originally from http://www.tecepe.com.br/nav/inav_met.htm by Omar Reis)
+    def sin(x):
+        return math.sin(math.radians(x))
+    def cos(x):
+        return math.cos(math.radians(x))
+    def tan(x):
+        return math.tan(math.radians(x))
+    def asin(x):
+        return math.degrees(math.asin(x))
+    def atan(x):
+        return math.degrees(math.atan(x))
     celestialObject = almanac.GetCelestialObject(celestialObjectName)
-    #(!)here must be instrument correction
-    Dip=0.0293*sqrt(hoh) #altitude correction, not same as in Omar Reis manual, but it provides data in decimal degrees for meters (from Bowditch)
-    deltat=deltats-Dip
-    Ro=0.0167/(tan(radians(deltat+7.31))/(deltat+4.4))#refraction correction. Do not measure objects which are near your astronomical horizon!
-    f=(0.28*P)/(T+273)
-    R=f*Ro
-    #(!)here must be oblateness correction
+    Dip=-0.0293*sqrt(aoe) #dip (observer altitude) correction, not same as in Bowditch , but it provides data in decimal degrees for meters
+    Sum=IC+Dip
+    Ha=Hs+Sum
+    R0=0.016667/tan(Ha+(7.31/(Ha+4.4))) #Bennet formula - from https://thenauticalalmanac.com/Formulas.html#Determine_Refraction_
+    f=(P/1013.25)*(283/(273.15+T)) #temperature correction formula, from https://en.wikipedia.org/wiki/Atmospheric_refraction (modified to match valves in Bowditch)
+    R=f*R0
     HP=celestialObject.HPAt(Y,M,D,h,m,s) #horizontal parallax of celestial object
-    PA=HP*cos(radians(deltat))
+    PA=HP*cos(Ha)
     SD=celestialObject.SDAt(Y,M,D,h,m,s) #semi-diameter of celestial object
-    deltato=deltat-R+PA-(limb*SD) #deltat observed; in case of measuring by lower limb of sun/moon it must be just "+SD". negative limb means lower limb, positive means upper limb
+    Ho=Ha-R+PA-(limb*SD) #deltat observed; in case of measuring by lower limb of sun/moon it must be just "+SD". negative limb means lower limb, positive means upper limb;    
     if not celestialObject.type=="Star":
         GHA=celestialObject.GHAAt(Y,M,D,h,m,s)
         delta=celestialObject.DecAt(Y,M,D,h,m,s)
@@ -24,11 +35,7 @@ def CalculateIntercept(phigce,lambdae,Y,M,D,h,m,s,celestialObjectName,deltats,ho
         delta=celestialObject.DecAt(Y,M,D,h,m,s)
         GHA=GHAAries+SHA
     LHA=GHA+lambdae
-    phigce=radians(phigce)
-    lambdae=radians(lambdae)
-    delta=radians(delta)
-    LHA=radians(LHA)
-    deltatc=degrees(asin((sin(phigce)*sin(delta))+(cos(phigce)*cos(delta)*cos(LHA))))
-    p=deltato-deltatc
-    z=angle.Normalize(degrees(atan(sin(LHA)/((sin(phigce)*cos(LHA))-(cos(phigce)*tan(delta))))))
-    return p,z
+    Hc=asin((sin(phie)*sin(delta))+(cos(phie)*cos(delta)*cos(LHA)))
+    a=Ho-Hc
+    Z=atan(sin(LHA)/((sin(phie)*cos(LHA))-(cos(phie)*tan(delta))))
+    return a,Z
--- tests/test_astrometry.py
+++ tests/test_astrometry.py
@@ -3,25 +3,28 @@ sys.path.append("..\src")
 
 import astrometry
 import angle
+import timeprocessor
 import unittest
 
 class test_astrometry(unittest.TestCase):
     def test_CalculateIntercept(self):
-        phigce=angle.ToDecimal("14*45'N")
-        lambdae=angle.ToDecimal("30*00'W")
-        Y=1974
-        M=9
+        phigce=angle.ToDecimal("39*00.0'N")
+        lambdae=angle.ToDecimal("45*26.0'W")
+        timeZone=timeprocessor.TimeZone(lambdae)
+        Y=2016
+        M=3
         D=9
-        h=21 #local 19
-        m=20
-        s=3
-        celestialObjectName="Arcturus"
-        deltats=angle.ToDecimal("28*29.5'")
-        hoh=31*0.3048
-        T=15.0
-        P=1013.25        
-        p,z=astrometry.CalculateIntercept(phigce,lambdae,Y,M,D,h,m,s,celestialObjectName,deltats,hoh,T,P)
-        print(p,z)
+        h=8+timeZone
+        m=58 #?? 45
+        s=27
+        #Y,M,D,h,m,s=timeprocessor.GMTtoUTC(Y,M,D,h,m,s,lambdae)
+        celestialObjectName="Deneb"
+        Hs=angle.ToDecimal("50*34.4'")
+        aoe=68*0.3048 #68 ft, to match dip correction in Bowditch
+        indexCorrection=0.2/60;
+        a,Z=astrometry.CalculateIntercept(phigce,lambdae,Y,M,D,h,m,s,celestialObjectName,Hs,aoe,IC=indexCorrection)
+        #print(angle.ToString(a))
+        #print(angle.ToString(Z))
         
 
 if __name__ == '__main__':
