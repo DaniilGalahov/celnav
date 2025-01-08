b007a1cd53fa689cd537afa159b2e0a14c20dabb
Daniil Galahov <daniil.galahov@yandex.ru>
2025-01-08 21:10:14 +0700

    Added chiangMae example and ToDo.
    Signed-off-by: Daniil Galahov <daniil.galahov@yandex.ru>

--- tests/practical/example_ChiangMai.py
+++ tests/practical/example_ChiangMai.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append("..\src")
 
 from math import sin, cos, acos, tan, atan, degrees, radians
-from astrometry import FindToCoEE
+from astrometry import FindToCoEE, ElevationCorrection
 from fix import TwoObjectFix
 from timeprocessor import TimeZone, LTtoGMT
 
@@ -19,6 +19,7 @@ lambdae=round(lambda0+(D*sin(radians(hdg))),0)
 
 print("DR",phigce,lambdae)
 
+celestialObjectName="Sun"
 aoe=(5*3)+1.5
 T=29
 
@@ -33,9 +34,10 @@ Y1,M1,D1,h1,m1,s1=LTtoGMT(Y1,M1,D1,h1,m1,s1,lambdae)
 a1=153
 b1=144
 c1=154
-Hs1=90.0-degrees(acos((pow(b1,2)+pow(c1,2)-pow(a1,2))/(2*a1*c1)))
+#Hs1=90.0-degrees(acos((pow(b1,2)+pow(c1,2)-pow(a1,2))/(2*a1*c1)))
 #print(Hs1)
 Hs1=28.484472 #from Horizon
+Hs1+=ElevationCorrection(celestialObjectName,Y1,M1,D1,h1,m1,s1,Hs1)#,hoe=aoe,T=T,limb=0)
 
 Y2=2024
 M2=12
@@ -48,25 +50,12 @@ Y2,M2,D2,h2,m2,s2=LTtoGMT(Y2,M2,D2,h2,m2,s2,lambdae)
 a2=165
 b2=136
 c2=154
-Hs2=90.0-degrees(acos((pow(b2,2)+pow(c2,2)-pow(a2,2))/(2*a2*c2)))
+#Hs2=90.0-degrees(acos((pow(b2,2)+pow(c2,2)-pow(a2,2))/(2*a2*c2)))
 #print(Hs2)
 Hs2=21.270918 #from Horizon
+Hs2+=ElevationCorrection(celestialObjectName,Y2,M2,D2,h2,m2,s2,Hs2)#,hoe=aoe,T=T,limb=0)
 
-p1,z1=FindToCoEE(phigce,lambdae,Y1,M1,D1,h1,m1,s1,"Sun",Hs1,aoe,T)
-p2,z2=FindToCoEE(phigce,lambdae,Y2,M2,D2,h2,m2,s2,"Sun",Hs2,aoe,T)
+p1,z1=FindToCoEE(phigce,lambdae,Y1,M1,D1,h1,m1,s1,celestialObjectName,Hs1)
+p2,z2=FindToCoEE(phigce,lambdae,Y2,M2,D2,h2,m2,s2,celestialObjectName,Hs2)
 phigc,lambda_=TwoObjectFix(phigce,lambdae,p1,z1,p2,z2)
 print(phigc,lambda_)
-'''
-iterN=0
-while True:
-    p1,z1=FindToCoEE(phigce,lambdae,Y1,M1,D1,h1,m1,s1,"Sun",Hs1,aoe,T)
-    p2,z2=FindToCoEE(phigce,lambdae,Y2,M2,D2,h2,m2,s2,"Sun",Hs2,aoe,T)
-    phigc,lambda_=TwoObjectFix(phigce,lambdae,p1,z1,p2,z2)
-    if (abs(phigce-phigc)<1e-6 and abs(lambdae-lambda_)<1e-6) or iterN>50:
-        break
-    else:
-        phigce=phigc
-        lambdae=lambda_
-        iterN=iterN+1
-print(phigc,lambda_)
-'''
