266dc917677c30e9f659fdb70e163e2f92799312
Daniil Galahov <daniil.galahov@yandex.ru>
2025-01-16 21:42:31 +0700

    Implemented Tienstra method
    Signed-off-by: Daniil Galahov <daniil.galahov@yandex.ru>

--- src/position.py
+++ src/position.py
@@ -1,4 +1,5 @@
 from external.math import *
+from external.astro import *
 
 import equationsolver as solve
 
@@ -15,3 +16,18 @@ def FromToCoEEo3CO(phiAP,lambdaAP,p1,gamma1,p2,gamma2,p3,gamma3):
     phigc=(phigc1+phigc2+phigc3)/3.0
     lambda_=(lambda1+lambda2+lambda3)/3.0
     return phigc,lambda_
+
+def FromP3Z3(Na,Ea,Nb,Eb,Nc,Ec,a,b,c): #phigc1,lambda1,phigc2,lambda2,phigc3,lambda3, rel. angle a, rel. angle b, rel. angle c; Realization of Tienstra method
+    vector_AB=vector([Eb,Nb])-vector([Ea,Na])
+    vector_BC=vector([Ec,Nc])-vector([Eb,Nb])
+    vector_CA=vector([Ea,Na])-vector([Ec,Nc])
+    A=angleBetween(vector_AB,-vector_CA)
+    B=angleBetween(-vector_AB,vector_BC)
+    C=angleBetween(-vector_BC,vector_CA)
+    n1=1.0/((1.0/tan(radians(A)))-(1.0/tan(radians(a))))
+    n2=1.0/((1.0/tan(radians(B)))-(1.0/tan(radians(b))))
+    n3=1.0/((1.0/tan(radians(C)))-(1.0/tan(radians(c))))
+    Np=((n1*Na)+(n2*Nb)+(n3*Nc))/(n1+n2+n3)
+    Ep=((n1*Ea)+(n2*Eb)+(n3*Ec))/(n1+n2+n3)
+    return Np,Ep
+    
--- tests/test_position.py
+++ tests/test_position.py
@@ -71,5 +71,21 @@ class test_position(unittest.TestCase):
         self.assertAlmostEqual(phi,phi0,2)
         self.assertAlmostEqual(lambda_,lambda0,1)
 
+    def test_FromP3Z3(self):
+        phi0=18.7666103 #Living Hills 2 Condo in Chiang Mai
+        lambda0=98.946801
+        phi1=18.8030882 #Doi Suthep Temple & mountain
+        lambda1=98.9213361
+        phi2=18.7594071 #Doi Kham Temple & mountain
+        lambda2=98.9158303
+        phi3=18.7531897 #Wat Pa Daed
+        lambda3=98.983521
+        a=143.8
+        b=143.6
+        c=72.6
+        phi,lambda_=position.FromP3Z3(phi1,lambda1,phi2,lambda2,phi3,lambda3,a,b,c)
+        self.assertAlmostEqual(phi,phi0,2)
+        self.assertAlmostEqual(lambda_,lambda0,2) #precision around 300 m.
+
 if __name__ == '__main__':
     unittest.main()
