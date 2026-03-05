87ff18db66daf89d1d20d44bba3bb09422d47902
Daniil Galahov <daniil.galahov@yandex.ru>
2026-03-06 00:58:27 +0700

    Added Dynamic class
    Signed-off-by: Daniil Galahov <daniil.galahov@yandex.ru>

--- src/extensions.py
+++ src/extensions.py
@@ -2,34 +2,62 @@
 #After practical testing can be added to main library
 
 from external.math import *
-from external.astro import JulianDate
-from astrometry import ElevationCorrection, FindToCoEE
+from external.astro import JulianDate, ROT3
+from timeprocessor import LTtoGMT
+from astrometry import FindToCoEE, ElevationCorrection
 from position import FromToCoEEo2CO
 
 
-#Sight reduction with elevation correction control
-def SightReductionCC(phiAP,lambdaAP,
-                     Y,M,D,h,m,s,
-                     celestialObjectName,
-                     el,
-                     correctElevation=True,
-                     indexCorrection=0,hoe=0,T=10,P=1010.0,limb=0):
-    if correctElevation:
-        el=el+ElevationCorrection(celestialObjectName,Y,M,D,h,m,s,el,indexCorrection,hoe,T,P,limb)
+#Sight reduction without elevation correction
+def CoarseSightReduction(phiAP,lambdaAP,
+                         Y,M,D,h,m,s,
+                         celestialObjectName,
+                         el):
     return FindToCoEE(phiAP,lambdaAP,Y,M,D,h,m,s,celestialObjectName,el)
 
-def DriftCorrection(Y1,M1,D1,h1,m1,s1,
-                    a1,Z1,
-                    Y2,M2,D2,h2,m2,s2,
-                    z,v):
-    z=radians(z)
-    Z1=radians(Z1)
-    JD1=JulianDate(Y1,M1,D1,h1,m1,s1)
-    JD2=JulianDate(Y2,M2,D2,h2,m2,s2)
-    dt=(JD2-JD1)*24.0
-    #print("dt:",dt)
-    d=(v*dt)/60.0
-    #print("d:", d)
-    a1=a1+(d*cos(z+Z1)) #why? Because we need to SUM (!!!) all affections on coordinates
-    #print("a1corr:",a1)
-    return a1
+#Functions for dynamic fix
+class Dynamic:
+    @staticmethod
+    def LTToGMT(Yl,Ml,Dl,hl,ml,sl,lambdaDR,APCorr):
+        lambdaDR=lambdaDR+APCorr[0]
+        return LTtoGMT(Yl,Ml,Dl,hl,ml,sl,lambdaDR)
+
+    @staticmethod
+    def TimeDelta(Y1,M1,D1,h1,m1,s1,
+                  Y2,M2,D2,h2,m2,s2):
+        JD1=JulianDate(Y1,M1,D1,h1,m1,s1)
+        JD2=JulianDate(Y2,M2,D2,h2,m2,s2)
+        dt=(JD2-JD1)*24.0
+        return dt
+
+    @staticmethod
+    def ZeroCorrection():
+        return vector([0,0,0])
+
+    @staticmethod
+    def APCorrection(z,v,dt):
+        z=radians(z)
+        d=(v*dt)/60.0
+        vector_N=vector([0,1,0])
+        vector_C=dot(ROT3(z),vector_N)*d
+        return vector_C
+
+    @staticmethod
+    def SightReduction(phiAP,lambdaAP,
+                       APCorr,
+                       Y,M,D,h,m,s,
+                       celestialObjectName,el,
+                       indexCorrection=0,hoe=0,T=10,P=1010.0,limb=0):
+        phiAP=phiAP+APCorr[1]
+        lambdaAP=lambdaAP+APCorr[0]
+        el=el+ElevationCorrection(celestialObjectName,Y,M,D,h,m,s,el,indexCorrection,hoe,T,P,limb)
+        return FindToCoEE(phiAP,lambdaAP,Y,M,D,h,m,s,celestialObjectName,el)
+
+    @staticmethod
+    def Fix(phiAP,lambdaAP,
+            APCorr,
+            a1,Z1,
+            a2,Z2):
+        phiAP=phiAP+APCorr[1]
+        lambdaAP=lambdaAP+APCorr[0]
+        return FromToCoEEo2CO(phiAP,lambdaAP,a1,Z1,a2,Z2)
--- tests/test_extensions.py
+++ tests/test_extensions.py
@@ -4,12 +4,12 @@ from math import cos, degrees
 from numpy import array as vector
 from numpy.linalg import norm as mag
 
-import celnav
-import extensions
+from celnav import SightReduction, LTToGMT
+from extensions import *
 import unittest
 
 class test_extensions(unittest.TestCase):
-    def test_SightReductionCC(self):
+    def test_CoarseSightReduction(self):
         phiAP=round(33.3562811) #Palomar observatory (precisely)
         lambdaAP=round(-116.8651156)
         celestialObjectName1="Mars"
@@ -20,28 +20,50 @@ class test_extensions(unittest.TestCase):
         m1=0
         s1=0
         el1=36.976441
-        a1,Z1=extensions.SightReductionCC(phiAP,lambdaAP,Y1,M1,D1,h1,m1,s1,celestialObjectName1,el1)
+        a1,Z1=SightReduction(phiAP,lambdaAP,Y1,M1,D1,h1,m1,s1,celestialObjectName1,el1)
         self.assertAlmostEqual(a1,0.12967432104721865,6)
         self.assertAlmostEqual(Z1,-84.7389551487631,6)
-        a1,Z1=extensions.SightReductionCC(phiAP,lambdaAP,Y1,M1,D1,h1,m1,s1,celestialObjectName1,el1,False)
+        a1,Z1=CoarseSightReduction(phiAP,lambdaAP,Y1,M1,D1,h1,m1,s1,celestialObjectName1,el1)
         self.assertAlmostEqual(a1,0.14870698327781895,6)
         self.assertAlmostEqual(Z1,-84.7389551487631,6)
 
-    def test_DriftCorrection(self):
-        def SunSightReductionViaLT(phiDR,lambdaDR,Yl,Ml,Dl,hl,ml,sl,el):
+    def test_TimeDelta(self):
+        dt=Dynamic.TimeDelta(2026,3,5,23,30,0,
+                             2026,3,6,0,30,0)
+        self.assertAlmostEqual(1.0,dt,6)
+
+    def test_APCorrection(self):
+        APCorr=Dynamic.APCorrection(45,10,0)
+        self.assertAlmostEqual(0.0,APCorr[0],6)
+        self.assertAlmostEqual(0.0,APCorr[1],6)
+        self.assertAlmostEqual(0.0,APCorr[2],6)
+
+        dt=Dynamic.TimeDelta(2026,3,5,14,00,0,
+                             2026,3,5,14,15,0)
+        APCorr=Dynamic.APCorrection(45,10,dt)
+        self.assertAlmostEqual(0.02946278211040981,APCorr[0],6)
+        self.assertAlmostEqual(0.02946278211040981,APCorr[1],6)
+        self.assertAlmostEqual(0.0,APCorr[2],6)
+
+    def test_Fix(self):
+        def SunSightReduction(phiDR,lambdaDR,
+                              APCorr,
+                              Yl,Ml,Dl,hl,ml,sl,
+                              el):
             #converting time to GMT
-            Y,M,D,h,m,s=celnav.LTToGMT(Yl,Ml,Dl,hl,ml,sl,lambdaDR)
+            Y,M,D,h,m,s=Dynamic.LTToGMT(Yl,Ml,Dl,hl,ml,sl,lambdaDR,APCorr)
 
             #preparing sight reduction
             celestialObjectName="Sun"
-            T=32.0    #environment temperature, degrees Celsius
-            P=1013.25 #environment pressure, millibars
+            T=32.0
+            P=1013.25
 
-            #do sight reduction
-            a,Z=celnav.SightReduction(phiDR, lambdaDR,
-                                      Y, M, D, h, m, s,
-                                      celestialObjectName, el,
-                                      T=T, P=P)
+            #do DYNAMIC sight reduction
+            a,Z=Dynamic.SightReduction(phiDR, lambdaDR,
+                                       APCorr,
+                                       Y, M, D, h, m, s,
+                                       celestialObjectName, el,
+                                       T=T, P=P)
             return a, Z
         
         #dead reckoning
@@ -60,6 +82,8 @@ class test_extensions(unittest.TestCase):
         s1=0
         el1=62.275364
 
+        APCorr1=Dynamic.ZeroCorrection()
+
         #Sight 2, at position [0;0.25]
         Y2=2026
         M2=3
@@ -69,18 +93,23 @@ class test_extensions(unittest.TestCase):
         s2=0
         el2=58.364564
 
-        #Calculating sight reductions
-        a1,Z1 = SunSightReductionViaLT(phiDR,lambdaDR,Y1,M1,D1,h1,m1,s1,el1)
-        a2,Z2 = SunSightReductionViaLT(phiDR,lambdaDR,Y2,M2,D2,h2,m2,s2,el2)
-        #print("a1:",a1)
+        dt=Dynamic.TimeDelta(Y1,M1,D1,h1,m1,s1,
+                             Y2,M2,D2,h2,m2,s2)
+        APCorr2=Dynamic.APCorrection(z,v,dt)
+        #print(APCorr2)
 
-        a1=extensions.DriftCorrection(Y1,M1,D1,h1,m1,s1,
-                                      a1,Z1,
-                                      Y2,M2,D2,h2,m2,s2,
-                                      z,v)
+        #Calculating sight reductions
+        a1,Z1 = SunSightReduction(phiDR,lambdaDR,
+                                  APCorr1,
+                                  Y1,M1,D1,h1,m1,s1,
+                                  el1)
+        a2,Z2 = SunSightReduction(phiDR,lambdaDR,
+                                  APCorr2,
+                                  Y2,M2,D2,h2,m2,s2,
+                                  el2)
 
         #Estimating geographical position
-        phi,lambda_=celnav.TwoObjectFix(phiDR,lambdaDR,a1,Z1,a2,Z2)
+        phi,lambda_=Dynamic.Fix(phiDR,lambdaDR,APCorr2,a1,Z1,a2,Z2)
         print("Calculated position lat/lon [dd]:",round(phi,3),round(lambda_,3))
 
         vector_pactual=vector([0.0,0.25])
@@ -92,6 +121,8 @@ class test_extensions(unittest.TestCase):
 
         #==============================================================
 
+        z=270.0 #straight W
+
         #Althernative Sight 2, at position [0;-0.25]
         Y2=2026
         M2=3
@@ -101,18 +132,20 @@ class test_extensions(unittest.TestCase):
         s2=0
         el2=58.854539
 
-        #Calculating sight reductions
-        a1,Z1 = SunSightReductionViaLT(phiDR,lambdaDR,Y1,M1,D1,h1,m1,s1,el1)
-        a2,Z2 = SunSightReductionViaLT(phiDR,lambdaDR,Y2,M2,D2,h2,m2,s2,el2)
-        #print("a1:",a1)
+        dt=Dynamic.TimeDelta(Y1,M1,D1,h1,m1,s1,
+                             Y2,M2,D2,h2,m2,s2)
 
-        a1=extensions.DriftCorrection(Y1,M1,D1,h1,m1,s1,
-                                      a1,Z1,
-                                      Y2,M2,D2,h2,m2,s2,
-                                      z,v)
+        APCorr2=Dynamic.APCorrection(z,v,dt)
+        #print(APCorr2)
+
+        #Calculating sight reductions
+        a2,Z2 = SunSightReduction(phiDR,lambdaDR,
+                                  APCorr2,
+                                  Y2,M2,D2,h2,m2,s2,
+                                  el2)
 
         #Estimating geographical position
-        phi,lambda_=celnav.TwoObjectFix(phiDR,lambdaDR,a1,Z1,a2,Z2)
+        phi,lambda_=Dynamic.Fix(phiDR,lambdaDR,APCorr2,a1,Z1,a2,Z2)
         print("Calculated position lat/lon [dd]:",round(phi,3),round(lambda_,3))
 
         vector_pactual=vector([0.0,-0.25])
